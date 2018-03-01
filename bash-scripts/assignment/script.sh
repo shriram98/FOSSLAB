effe127913edb2e71592ec3659329af622f3947b
Shriram <shriramvenugopal123@gmail.com>
2018-03-01 19:46:21 +0530

    calculated cgpa

--- bash-scripts/assignment/script.sh
+++ bash-scripts/assignment/script.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-#converting to text file
-pdftotext -layout  result.pdf result.txt
+#function to calculate sgpa with argument as 1 for s1 and 2 for s2
 
-#filtering list of cs students and pasting it in resultcs.txt
-paste <(grep MDL16CS result.txt) <(grep CS110\( result.txt) | tr -d ' ' > resultcs.txt
+function calculateSGPA {
+  
+  pdftotext -layout result$1.pdf result$1.txt
+  tr -d '\014' <result$1.txt > temp.txt
+  mv temp.txt result$1.txt
 
-#removing temporary files
-rm grades.txt 2>/dev/null
-touch grades.txt
-rm sgpa.txt 2>/dev/null
-touch sgpa.txt
+  #filtering list of cs students and pasting it in resultcs.txt
+  paste <(grep MDL16CS result$1.txt) <(grep CS1$10\( result$1.txt) | tr -d ' ' > resultcs.txt
 
-#for loop to get grades of each student and paste it in grades.txt
-for i in {2..10}
-do
-  paste grades.txt  <(cut -d'(' -f$i resultcs.txt | cut -d')' -f1) > temp.txt
-  mv temp.txt grades.txt
-done
+  touch grades.txt
+  touch sgpa.txt
 
-#calculating sgpa from grades.txt and storing it in sgpa.txt
-#here bc() is used to do calculations in floating point as bash does not support flop
-credits=(4 4 3 3 3 3 1 1 1)
-while read -r line
-do
-   sgpa=0
-   for i in {2..10}
-   do
-     case "$(echo $line | cut -d' ' -f$i )" in
+  #for loop to get grades of each student and paste it in grades.txt
+  for i in {2..10}
+  do
+    paste grades.txt  <(cut -d'(' -f$i resultcs.txt | cut -d')' -f1) > temp.txt
+    mv temp.txt grades.txt
+  done
+
+  #calculating sgpa from grades.txt and storing it in sgpa.txt
+  #here bc() is used to do calculations in floating point as bash does not support flop
+
+  if [ $1 -eq 1 ]
+  then
+    credits=(4 4 3 3 3 3 1 1 1)
+  else
+    credits=(4 4 3 1 1 4 3 3 1)
+  fi
+
+  while read -r line
+  do
+    sgpa=0
+    for i in {2..10}
+    do
+      case "$(echo $line | cut -d' ' -f$i )" in
        O) sum=$(bc <<< "scale = 2; (10 * ${credits[i-2]})")
          ;;
        A+) sum=$(bc <<< "scale = 2; (9 * ${credits[i-2]})")
@@ -49,15 +58,33 @@ do
      esac
      sgpa=$(bc <<< "scale = 2; ($sgpa + $sum) ")         
    done
-   bc <<< "scale = 2; ($sgpa/23)" >> sgpa.txt
-done<grades.txt
+    bc <<< "scale = 2; ($sgpa/(23 + $1 -1))" >> sgpa.txt
+  done<grades.txt
 
-#getting csb student list
-wget http://14.139.184.212/ask/c4b/c4b.txt
+  #getting csb student list
+  #wget http://14.139.184.212/ask/c4b/c4b.txt
 
-#filtering only csb student sgpa and storing in SGPA.txt
-join <(cut -d$'\t' -f4 c4b.txt) <(paste <(grep MDL16CS result.txt | cut -d' ' -f1) sgpa.txt) > SGPA.txt 2>/dev/null 
+  #filtering only csb student sgpa and storing in SGPA.txt
+  join <(cut -d$'\t' -f4 c4b.txt) <(paste <(grep MDL16CS result$1.txt | cut -d' ' -f1) sgpa.txt) > SGPA$1.txt 2>/dev/null 
 
-#removing temporary files
-rm resultcs.txt sgpa.txt grades.txt result.txt
+  #removing temporary files
+  rm resultcs.txt sgpa.txt grades.txt result$1.txt
+  }
 
+calculateSGPA 1
+calculateSGPA 2
+
+join SGPA1.txt <(cut -f2 SGPA2.txt) > SGPA.txt
+rm SGPA1.txt SGPA2.txt
+
+rm CGPA.txt 2>/dev/null
+touch CGPA.txt
+
+while read -r line
+do
+  bc <<< " scale = 2; ((($( echo $line | cut -d' ' -f2) * 23) + ($( echo $line | cut -d' ' -f3) * 24)) / 47)" >> CGPA.txt
+done<SGPA.txt
+
+
+paste <(cut -d' ' -f1 SGPA.txt) CGPA.txt > temp.txt
+mv temp.txt CGPA.txt
